4990545ce3b22a9b19ffb848e90da42078993679
”Artsiom <artsiomdziominski@gmail.com>
2022-10-06 16:17:56 +0400

    HTML and SCSS code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MainComponent } from './pages/main/main.component';
+import { OrderComponent } from './pages/order/order.component';
+import { StatisticsComponent } from './pages/statistics/statistics.component';
+import { BlockPriceComponent } from './components/block-price/block-price.component';
+import { OrderRowComponent } from './components/order-row/order-row.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    OrderComponent,
+    StatisticsComponent,
+    BlockPriceComponent,
+    OrderRowComponent,
   ],
   imports: [
     BrowserModule
