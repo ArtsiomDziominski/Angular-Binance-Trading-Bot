04dcd32d967a7eb36e8eb8e9edd10bc172f03fe0
”Artsiom <artsiomdziominski@gmail.com>
2022-10-07 16:41:55 +0400

    add routing and lazy loading

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'Binance-Trading-Bot';
+export class AppComponent implements OnInit {
+  public isOpenDialogBox: boolean = false;
+  public isInfoSaveStorageBox: boolean = true;
+
+  public ngOnInit() {
+
+  }
+
+  public updateOpenDialogBox($event: boolean) {
+    this.isOpenDialogBox = $event;
+  }
 }
