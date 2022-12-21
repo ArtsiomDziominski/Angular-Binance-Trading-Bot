a6978e06cbe32cf87be308324e4d0dfa7e43d507
Artsiom Dziominski <artsiomdziominski@gmail.com>
2022-12-21 15:36:30 +0400

    install server heroku

--- src/app/const/http-request.ts
+++ src/app/const/http-request.ts
@@ -1,5 +1,5 @@
 export const LURL:string = '';//'http://localhost:3000'
-export const BURL:string = 'http://185.104.115.27:3000'
+export const BURL:string = 'https://185.104.115.27:3000'
 export const HTTP_GET_24hr: string = 'https://fapi.binance.com/fapi/v1/ticker/24hr'
 export const BALANCE: string = '/balance/';
 export const INCOME_HISTORY: string = '/income-history/';
